821b529dc14c028b4e919453fe8e0ae86cd3c332
Michael Einzinger <ITN243871@BBRZ.at>
2024-07-24 14:56:04 +0200

    second version

--- employeeClass.js
+++ employeeClass.js
@@ -1,14 +1,28 @@
 const User = require('./userClass');
 
 class Employee extends User {
-    constructor(name, position) {
+    constructor(name, position = "new Employee", salary = 0) {
         super(name, "Employee");
         this._position = position;
+        this._salary = salary;
     }
 
     get position() {
         return this._position;
     }
+
+    set position(newPosition) {
+        this._position = newPosition;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(newSalary) {
+        this._salary = newSalary
+    }
+
 }
 
 module.exports = Employee;
--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const saveJSON = (filename, data) => {
 };
 
 // CREATE USERS AND ACCOUNTS
-const data = loadJSON("data.json")[0];
+const data = loadJSON("data.json");
 const customers = [];
 const employees = [];
 const accounts = [];
@@ -41,48 +41,201 @@ data.customers.forEach((customerData) => {
 });
 
 data.employees.forEach((employeeData) => {
-  const employee = new Employee(employeeData.name, employeeData.position);
+  const employee = new Employee(
+    employeeData.name,
+    employeeData.position,
+    employeeData.salary
+  );
   employees.push(employee);
 });
 
-// VARIABLES
+// VARIABLES AND CONSTANTS
 let current_user = null;
 let commands;
+
+const formatCurrency = (number) => {
+  return number.toLocaleString("de-DE", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+};
 
 // ----------------------------------------------------------------------------------------------------------------------
 
 // FUNCTIONS
 
 let accountlist = () => {
-  console.log("\nACCOUNT  | OWNER                     | BALANCE");
-  console.log("---------+---------------------------+---------");
+  let column1Length = 10;
+  let column2Length = 25;
+  let column3Length = 18;
+  let column1Text = "ACCOUNT";
+  let column2Text = "OWNER";
+  let column3Text = "BALANCE";
+
+  console.log(
+    "\n" +
+      column1Text +
+      " ".repeat(column1Length - column1Text.length) +
+      "| " +
+      column2Text +
+      " ".repeat(column2Length - column2Text.length - 1) +
+      "| " +
+      column3Text
+  );
+
+  console.log(
+    "-".repeat(column1Length) +
+      "+" +
+      "-".repeat(column2Length) +
+      "+" +
+      "-".repeat(column3Length)
+  );
+
   accounts.forEach((element) => {
     console.log(
       `${
-        element.account_number + " ".repeat(9 - element.account_number.length)
+        element.account_number +
+        " ".repeat(column1Length - element.account_number.length)
       }| ${
-        element.customer.name + " ".repeat(26 - element.customer.name.length)
-      }| € ${element.balance}`
+        element.customer.name +
+        " ".repeat(column2Length - element.customer.name.length - 1)
+      }| € ${formatCurrency(element.balance)}`
     );
   });
   console.log();
 };
 
-let userlist = () => {
-  console.log("\nCUSTOMERS\n=========");
-  customers.forEach((element) => {
-    console.log("- " + element.name);
-  });
-  console.log("\nEMPLOYEES\n=========");
+
+let employeelist = () => {
+  let column1Length = 15;
+  let column2Length = 30;
+  let column3Length = 18;
+  let column1Text = "EMPLOYEE";
+  let column2Text = "POSITION";
+  let column3Text = "SALARY";
+
+  console.log(
+    "\n" +
+      column1Text +
+      " ".repeat(column1Length - column1Text.length) +
+      "| " +
+      column2Text +
+      " ".repeat(column2Length - column2Text.length - 1) +
+      "| " +
+      column3Text
+  );
+
+  console.log(
+    "-".repeat(column1Length) +
+      "+" +
+      "-".repeat(column2Length) +
+      "+" +
+      "-".repeat(column3Length)
+  );
+
   employees.forEach((element) => {
-    console.log(`- ${element.name} (${element.position})`);
+    console.log(
+      `${element.name + " ".repeat(column1Length - element.name.length)}| ${element.position + " ".repeat(column2Length - element.position.length - 1)}| € ${formatCurrency(element.salary)}`);
   });
   console.log();
 };
 
-let create = (new_user) => {
-  customers.push(new Customer(new_user));
-  console.log("New user " + new_user + " has been created.\n");
+let customerlist = () => {
+  let column1Length = 18;
+  let column2Length = 20;
+  let column3Length = 18;
+  let column1Text = "CUSTOMER";
+  let column2Text = "NO. OF ACCOUNTS";
+  let column3Text = "TOTAL BALANCE";
+
+  console.log(
+    "\n" +
+      column1Text +
+      " ".repeat(column1Length - column1Text.length) +
+      "| " +
+      column2Text +
+      " ".repeat(column2Length - column2Text.length - 1) +
+      "| " +
+      column3Text
+  );
+
+  console.log(
+    "-".repeat(column1Length) +
+      "+" +
+      "-".repeat(column2Length) +
+      "+" +
+      "-".repeat(column3Length)
+  );
+
+  customers.forEach((element) => {
+    let customerAccounts = accounts.filter(
+      (account) => account.customer === element
+    );
+    let accountCount = customerAccounts.length;
+    let totalBalance = customerAccounts.reduce(
+      (sum, account) => sum + account.balance,
+      0
+    );
+
+    console.log(
+      `${element.name + " ".repeat(column1Length - element.name.length)}| ${
+        accountCount +
+        " ".repeat(column2Length - accountCount.toString.length - 1)
+      }| € ${formatCurrency(totalBalance)}`
+    );
+  });
+  console.log();
+};
+
+let create = (role, new_user) => {
+  const userExists =
+    customers.some((customer) => customer.name === new_user) ||
+    employees.some((employee) => employee.name === new_user);
+
+  if (userExists) {
+    console.log("ERROR - Username already exists.\n");
+    return;
+  }
+
+  switch (role) {
+    case "customer":
+      customers.push(new Customer(new_user));
+      console.log("New user " + new_user + " has been created.\n");
+      break;
+    case "employee":
+      employees.push(new Employee(new_user));
+      console.log("New employee " + new_user + " has been created.\n");
+      break;
+    default:
+      console.log('ERROR - Accepted roles are "customer" or "employee"!\n');
+      break;
+  }
+};
+
+let setposition = (username, position) => {
+  let employee = employees.find((employee) => employee.name == username);
+  if (employee) {
+    employee.position = position;
+    console.log(
+      `Position of ${employee.name} changed to ${employee.position}\n`
+    );
+  } else {
+    console.log("Employee not found.\n");
+  }
+};
+
+let setsalary = (username, salary) => {
+  let employee = employees.find((employee) => employee.name == username);
+  if (employee) {
+    employee.salary = parseFloat(salary);
+    console.log(
+      `Salary of ${employee.name} changed to € ${formatCurrency(
+        employee.salary
+      )}\n`
+    );
+  } else {
+    console.log("Employee not found.\n");
+  }
 };
 
 let login = (username) => {
@@ -107,7 +260,7 @@ let login = (username) => {
 let logout = () => {
   current_user = null;
   console.log("Logged out.\n");
-}
+};
 
 let myaccounts = () => {
   if (!current_user) {
@@ -120,7 +273,11 @@ let myaccounts = () => {
   numberOfAccounts = 0;
   accounts.forEach((element) => {
     if (element.customer.name == current_user.name) {
-      console.log(`Account ${element.account_number}: € ${element.balance} `);
+      console.log(
+        `Account ${element.account_number}: € ${formatCurrency(
+          element.balance
+        )} `
+      );
       numberOfAccounts++;
     }
   });
@@ -210,7 +367,11 @@ let deposit = (account_number, amount) => {
       element.balance = new_balance;
 
       console.log(
-        `The amount of € ${amount} has been added to your account ${element.account_number}.\nYour new balance is € ${element.balance}.\n`
+        `The amount of € ${formatCurrency(
+          amount
+        )} has been added to your account ${
+          element.account_number
+        }.\nYour new balance is € ${formatCurrency(element.balance)}.\n`
       );
     }
   });
@@ -222,7 +383,7 @@ let deposit = (account_number, amount) => {
 
 let withdraw = (account_number, amount) => {
   if (!current_user) {
-    console.log("You must be logged in to withdraw.\n");
+    console.log("You must be logged in to withdraw money.\n");
     return;
   }
 
@@ -248,7 +409,11 @@ let withdraw = (account_number, amount) => {
       element.balance = new_balance;
 
       console.log(
-        `The amount of € ${amount} has been withdrawn from your account ${element.account_number}.\nYour new balance is € ${element.balance}.\n`
+        `The amount of € ${formatCurrency(
+          amount
+        )} has been withdrawn from your account ${
+          element.account_number
+        }.\nYour new balance is € ${formatCurrency(element.balance)}.\n`
       );
     }
   });
@@ -300,7 +465,13 @@ let transfer = (my_account_number, target_account_number, amount) => {
     target_account.balance += amount;
 
     console.log(
-      `The amount of € ${amount} has been successfully transfered from your account ${my_account.account_number} to account ${target_account.account_number}.\nYour new balance is € ${my_account.balance}.\n`
+      `The amount of € ${formatCurrency(
+        amount
+      )} has been successfully transfered from your account ${
+        my_account.account_number
+      } to account ${
+        target_account.account_number
+      }.\nYour new balance is € ${formatCurrency(my_account.balance)}.\n`
     );
   }
 
@@ -325,10 +496,11 @@ let save = () => {
     employees: employees.map((employee) => ({
       name: employee.name,
       position: employee.position,
+      salary: employee.salary,
     })),
   };
 
-  saveJSON("data.json", [dataToSave]);
+  saveJSON("data.json", dataToSave);
   console.log("Data saved to data.json\n");
 };
 
@@ -336,41 +508,101 @@ let whoami = () => {
   current_user
     ? console.log("You are logged in as user " + current_user.name + ".\n")
     : console.log("You are not logged in!\n");
-}
+};
 
 let help = () => {
   console.log("\nPossible commands are:\n");
-  console.log("COMMAND             | DESCRIPTION                             | PARAMETERS | EXAMPLE");
-  console.log("--------------------+-----------------------------------------+------------+----------------------------------------------");
+
+  let column1Length = 17;
+  let column2Length = 45;
+  let column3Length = 12;
+  let column4Length = 55;
+  let column1Text = "COMMAND";
+  let column2Text = "DESCRIPTION";
+  let column3Text = "PARAMETERS";
+  let column4Text = "EXAMPLE";
+
+  console.log(
+      column1Text +
+      " ".repeat(column1Length - column1Text.length) +
+      "| " +
+      column2Text +
+      " ".repeat(column2Length - column2Text.length - 1) +
+      "| " +
+      column3Text +
+      " ".repeat(column3Length - column3Text.length - 1) +
+      "| " +
+      column4Text
+  );
+
+  console.log(
+    "-".repeat(column1Length) +
+      "+" +
+      "-".repeat(column2Length) +
+      "+" +
+      "-".repeat(column3Length) +
+      "+" +
+      "-".repeat(column4Length)
+  );
+
   for (let cmd in commands) {
     console.log(
-      `${
-        cmd + " ".repeat(20 - cmd.length)
+      `${cmd + " ".repeat(column1Length - cmd.length)}| ${
+        commands[cmd].description +
+        " ".repeat(column2Length - commands[cmd].description.length - 1)
       }| ${
-        commands[cmd].description + " ".repeat(40 - commands[cmd].description.length)
-      }| ${
-        commands[cmd].neededParameters  + " ".repeat(11 - commands[cmd].neededParameters.toString.length)
-      }| ${
-        (commands[cmd].example) ? commands[cmd].example : ""
-      }`
+        commands[cmd].neededParameters +
+        " ".repeat(column3Length - commands[cmd].neededParameters.toString.length - 1)
+      }| ${commands[cmd].example ? commands[cmd].example : ""}`
     );
   }
   console.log();
-}
+};
 
 let exit = () => {
   console.log("Exiting program...\n");
   rl.close();
   process.exit(0);
-}
+};
 
+// TEST HILFSFUNKTION FÜR TABELLEN
 let test = () => {
-  console.log("CUSTOMERS:");
-  console.log(customers);
-  console.log("EMPLOYEES:");
-  console.log(employees);
-  console.log("DATA:");
-  console.log(data);
+  const createTable = (headers, columnLengths, rows) => {
+
+    let headerRow = headers
+      .map((header, index) => header + " ".repeat(columnLengths[index] - header.length))
+      .join(" | ");
+    console.log("\n" + headerRow);
+  
+
+    let separatorRow = columnLengths.map(length => "-".repeat(length)).join("-+-");
+    console.log(separatorRow);
+  
+
+    rows.forEach(row => {
+      let rowText = row
+        .map((cell, index) => cell + " ".repeat(columnLengths[index] - cell.length))
+        .join(" | ");
+      console.log(rowText);
+    });
+  
+    console.log();
+  };
+  
+    let headers = ["CUSTOMER", "NO. OF ACCOUNTS", "TOTAL BALANCE"];
+    let columnLengths = [18, 20, 18];
+    let rows = customers.map(customer => {
+      let customerAccounts = accounts.filter(account => account.customer === customer);
+      let accountCount = customerAccounts.length;
+      let totalBalance = customerAccounts.reduce((sum, account) => sum + account.balance, 0);
+      return [
+        customer.name,
+        accountCount.toString(),
+        `€ ${formatCurrency(totalBalance)}`
+      ];
+    });
+  
+    createTable(headers, columnLengths, rows);
 };
 
 // ----------------------------------------------------------------------------------------------------------------------
@@ -396,115 +628,141 @@ let executeCommand = (input) => {
     accountlist: {
       neededParameters: 0,
       description: "List all accounts",
-      action: () => accountlist()
+      action: () => accountlist(),
     },
 
-    userlist: {
+    employees: {
       neededParameters: 0,
-      description: "List all customers and employees",
-      action: () => userlist()
+      description: "List all employees",
+      action: () => employeelist(),
+    },
+
+    customers: {
+      neededParameters: 0,
+      description: "List all customers",
+      action: () => customerlist(),
     },
 
     create: {
-      neededParameters: 1,
-      description: "Create a new customer",
-      example: "create [username]",
-      action: (username) => create(username)
+      neededParameters: 2,
+      description: "Create a new customer or employee",
+      example: "create customer/employee [username]",
+      action: (role, username) => create(role, username),
     },
 
     login: {
       neededParameters: 1,
       description: "Login as a customer",
       example: "login [username]",
-      action: (username) => login(username)
+      action: (username) => login(username),
     },
 
     myaccounts: {
       neededParameters: 0,
       description: "List all your accounts",
-      action: () => myaccounts()
+      action: () => myaccounts(),
     },
 
     open: {
       neededParameters: 0,
       description: "Open a new account",
-      action: () => open()
+      action: () => open(),
     },
 
     close: {
       neededParameters: 1,
       description: "Close an existing account",
       example: "close [accountnumber]",
-      action: (accountnumber) => close(accountnumber)
+      action: (accountnumber) => close(accountnumber),
     },
 
     deposit: {
       neededParameters: 2,
       description: "Deposit money into an account",
       example: "deposit [accountnumber] [amount]",
-      action: (accountnumber, amount) => deposit(accountnumber, parseFloat(amount))
+      action: (accountnumber, amount) =>
+        deposit(accountnumber, parseFloat(amount)),
     },
 
     withdraw: {
       neededParameters: 2,
       description: "Withdraw money from an account",
       example: "withdraw [accountnumber] [amount]",
-      action: (accountnumber, amount) => withdraw(accountnumber, parseFloat(amount))
+      action: (accountnumber, amount) =>
+        withdraw(accountnumber, parseFloat(amount)),
     },
 
     transfer: {
       neededParameters: 3,
       description: "Transfer money between accounts",
       example: "transfer [accountnumber1] [accountnumber2] [amount]",
-      action: (accountnumber, targetAccount, amount) => transfer(accountnumber, targetAccount, parseFloat(amount))
+      action: (accountnumber, targetAccount, amount) =>
+        transfer(accountnumber, targetAccount, parseFloat(amount)),
     },
 
     whoami: {
       neededParameters: 0,
       description: "Display the logged in user",
-      action: () => whoami()
+      action: () => whoami(),
     },
 
     logout: {
       neededParameters: 0,
       description: "Logout the current user",
-      action: () => logout()
+      action: () => logout(),
     },
 
     save: {
       neededParameters: 0,
       description: "Save data to JSON file",
-      action: () => save()
+      action: () => save(),
+    },
+
+    setposition: {
+      neededParameters: 2,
+      description: "Changes the position of an employee",
+      example: "setposition [username] [new_position]",
+      action: (username, position) => setposition(username, position),
+    },
+
+    setsalary: {
+      neededParameters: 2,
+      description: "Changes the salary of an employee",
+      example: "setsalary [username] [new_salary]",
+      action: (username, salary) => setsalary(username, salary),
     },
 
     help: {
       neededParameters: 0,
       description: "Display this help message",
-      action: () => help()
+      action: () => help(),
     },
 
-    test: {
-      neededParameters: 0,
-      description: "[for testing purpose only]",
-      action: () => test()
-    },
+    // test: {
+    //   neededParameters: 0,
+    //   description: "[for testing purpose only]",
+    //   action: () => test(),
+    // },
 
     exit: {
       neededParameters: 0,
       description: "Exit the program",
-      action: () => exit()
-    }
+      action: () => exit(),
+    },
   };
 
   if (commands[command.toLowerCase()]) {
-    action(commands[command.toLowerCase()].action, commands[command.toLowerCase()].neededParameters, params);
+    action(
+      commands[command.toLowerCase()].action,
+      commands[command.toLowerCase()].neededParameters,
+      params
+    );
   } else {
     console.log("ERROR: Unknown Command\n");
   }
-  
+
   rl.prompt();
 };
-
 
 // ----------------------------------------------------------------------------------------------------------------------
 
@@ -512,7 +770,7 @@ let executeCommand = (input) => {
 
 console.log("\nBANK-MANAGEMENT-SYSTEM");
 console.log("======================\n");
-console.log("INFO: type \"help\" for an overview of the available commands!\n");
+console.log('INFO: type "help" for an overview of the available commands!\n');
 rl.setPrompt("Enter command: ");
 rl.prompt();
 
